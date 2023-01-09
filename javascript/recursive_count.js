ace9691c800d436878a6900f388d9ea5497e1e97
Chris Li <chris.lty07@gmail.com>
2023-01-09 11:21:49 -0500

    commit tree ef4bcc54e256b4d1f11e1ecf6c4b13082be692a8

--- javascript/recursive_count.js
+++ javascript/recursive_count.js
@@ -1,12 +1,22 @@
 function recursiveCount(num = 0) {
-  // type your code here
+  // this needs to count up from 0 to 9. stops at 9
+  if (num > 9) {
+    return
+  } else {
+    console.log(num)
+    // it needs to call itself
+    recursiveCount(num + 1)
+  }
 }
+
+// recursiveCount()
 
 if (require.main === module) {
   recursiveCount();
 }
 
 module.exports = recursiveCount;
+
 
 // OPTIONAL
 // Please add your pseudocode to this file
